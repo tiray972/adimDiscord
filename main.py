b1e3d9aaeb9735f0ebd17fcd30859428ff6c2f2f
rayan <tiray972@gmail.com>
2022-03-10 15:23:29 -0400

    Modifications qui seront validées : 	modifié :         main.py

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ from math import *
 import os
 
 def recupdermessage():
-	token = input('entre le token')
+	token = 'OTEyNDg2ODc2NDEyMjc2Nzc3.YZwpoA.ih-tIjzWEQobgs54y6mjAaVj3J8'
 	client = discord.Client()
 	@client.event
 	async def on_ready():
 		print(f'{client.user} has connected to Discord!')
-		mes=open('message.txt','+w')
+		mes=open('myGitFile/projectPerso/adminDiscord/message.txt','+w')
 		channel = client.get_channel(912495325997064193)
 		message = await channel.fetch_message(channel.last_message_id)
-		nomUtilisateur = await channel.fetch_message(channel.last_message_id)
-		mes.write(message.content)
+		nomUtilisateur =  message.author
+		nomUtilisateur = nomUtilisateur.id
+		mes.write(message.content+'\n')
+		mes.write(str(nomUtilisateur))
 		user = await client.fetch_user(711967096820465777)
 		await client.close()
 		time.sleep(1)
 	client.run(token)
-	mes=open('message.txt','+r')
+	mes=open('myGitFile/projectPerso/adminDiscord/message.txt','+r')
 	
 	return mes.read()
 
@@ -29,4 +31,6 @@ def reconaissance():
 
 def createChanel():
     pass
+
+
 recupdermessage()
