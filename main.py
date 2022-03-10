4594aab5df2c0af527218aa04f8a46a842bfe61e
rayan <tiray972@gmail.com>
2022-03-10 10:23:06 -0400

    Modifications qui seront validées : 	modifié :         main.py

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import discord
+import time
+from math import *
+import os
+
+def recupdermessage():
+	token = ""
+	client = discord.Client()
+	@client.event
+	async def on_ready():
+		print(f'{client.user} has connected to Discord!')
+		mes=open('message.txt','+w')
+		channel = client.get_channel(912495325997064193)
+		message = await channel.fetch_message(channel.last_message_id)
+		mes.write(message.content)
+		user = await client.fetch_user(711967096820465777)
+		await client.close()
+		time.sleep(1)
+	client.run(token)
+	mes=open('message.txt','+r')
+	
+	return mes.read()
+
+
 
 def reconaissance():
     pass
