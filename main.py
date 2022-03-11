224d9261bff2b98c04e148d54a81b685e215a808
rayan <tiray972@gmail.com>
2022-03-10 23:50:17 -0400

    Modifications qui seront validées : 	nouveau fichier : fichier_export.csv 	modifié :         main.py

--- main.py
+++ main.py
@@ -2,18 +2,27 @@ import discord
 import time
 from math import *
 import os
-
+import csv
+chemin = "./myGitFile/projectPerso/adminDiscord/fichier_export.csv"
 def recupdermessage():
 	token = input('entre ton token :')
 	client = discord.Client()
 	@client.event
 	async def on_ready():
 		print(f'{client.user} has connected to Discord!')
-		mes=open('myGitFile/projectPerso/adminDiscord/message.txt','+w')
 		channel = client.get_channel(912495325997064193)
+		mes=open('myGitFile/projectPerso/adminDiscord/message.txt','+w')
 		message = await channel.fetch_message(channel.last_message_id)
 		nomUtilisateur =  message.author
 		idUtilisateur = nomUtilisateur.id
+		
+		with open(chemin, mode='a') as mon_fichier:
+			mon_fichier_ecrire = csv.writer(mon_fichier, delimiter=',',
+											quotechar='"',
+											quoting=csv.QUOTE_MINIMAL)
+			mon_fichier_ecrire.writerow([nomUtilisateur, idUtilisateur, message.content, 0])	
+		
+		
 		mes.write(message.content+'\n')
 		mes.write(str(nomUtilisateur))
 		user = await client.fetch_user(711967096820465777)
