ca1f108db90851166a1cbbeb8f5535c1ff861950
rayan <tiray972@gmail.com>
2022-03-11 17:13:25 -0400

    Modifications qui seront validées : 	modifié :         fichier_export.csv 	nouveau fichier : gestion.py

---
+++ gestion.py
@@ -0,0 +1,11 @@
+from nbformat import read
+import numpy
+
+
+import numpy as np
+import pandas as pd
+import discord
+
+
+df=pd.read_csv('./myGitFile/projectPerso/adminDiscord/fichier_export.csv',sep=',')
+print(df)
